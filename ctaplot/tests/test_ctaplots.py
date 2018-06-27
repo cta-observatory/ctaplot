1625b912e7d40aa56525d0c173061c43cb4ab737
Vuillaume <thomas.vuillaume@lapp.in2p3.fr>
2018-06-27 16:34:56 +0200

    adding test on plot perf and requirements

--- ctaplot/tests/test_ctaplots.py
+++ ctaplot/tests/test_ctaplots.py
@@ -25,3 +25,30 @@ def test_saveplot_energy_resolution():
     assert os.path.isfile("eres.png")
     os.remove("eres.png")
 
+
+def test_plot_energy_resolution_cta_performances():
+    plots.plot_energy_resolution_cta_performances('north', color='green')
+
+
+def test_plot_angular_resolution_cta_performances():
+    plots.plot_angular_res_cta_performance('north', color='green')
+
+
+def test_plot_angular_resolution_requirements():
+    plots.plot_angular_res_requirement('north', color='green')
+
+
+def test_plot_effective_area_performances():
+    plots.plot_effective_area_performances('north', color='green')
+
+
+def test_plot_effective_area_requirement():
+    plots.plot_effective_area_requirement('north', color='green')
+
+
+def test_plot_sensitivity_performances():
+    plots.plot_sensitivity_performances('north', color='green')
+
+
+def test_plot_sensitivity_requirement():
+    plots.plot_sensitivity_requirement('north', color='green')
