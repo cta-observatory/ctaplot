01f5ff9772273e148d57c981e358e15d73a69982
vuillaut <thomas.vuillaume@gmail.com>
2019-05-09 14:53:42 +0200

    v0.22

--- setup.py
+++ setup.py
@@ -16,7 +16,7 @@ dataset = package_files('share')
 print("dataset {}".format(dataset))
 
 setup(name='ctaplot',
-      version=0.21,
+      version=0.22,
       description="DESCRIPTION",
       # these should be minimum list of what is needed to run (note
       # don't need to list the sub-dependencies like numpy, since
