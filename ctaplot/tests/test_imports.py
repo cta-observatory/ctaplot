b4d54bde647eaee12511de07e51bd5295184f8f8
vuillaut <thomas.vuillaume@gmail.com>
2019-09-04 14:54:07 +0200

    test imports in one file

---
+++ ctaplot/tests/test_imports.py
@@ -0,0 +1,23 @@
+def test_import_plots():
+    try:
+        from ctaplot import plots
+    except:
+        raise()
+
+def test_import_ana():
+    try:
+        from ctaplot import ana
+    except:
+        raise ()
+
+def test_import_dataset():
+    try:
+        from ctaplot import dataset
+    except:
+        raise ()
+
+def test_import_gammaboard():
+    try:
+        from ctaplot import gammaboard
+    except:
+        raise()
