e729b9411f94704dce4c22990242d5e7ff72af4f
vuillaut <thomas.vuillaume@gmail.com>
2019-09-04 14:45:00 +0200

    version 0.3.2

--- ctaplot/__init__.py
+++ ctaplot/__init__.py
@@ -1,9 +1,7 @@
 from .plots import *
-from .plotcamera import *
 from .ana import *
 from .dataset import *
 from .io import *
 from .gammaboard import gammaboard
 
-
-__version__ = '0.4.0'
+__version__ = '0.3.2'
