849c6ff9c2c892f3df9afae11e3ae0aac8f2eef5
vuillaut <thomas.vuillaume@gmail.com>
2019-10-16 16:10:03 +0200

    adding unit tests

---
+++ ctaplot/tests/test_dataset.py
@@ -0,0 +1,17 @@
+from ctaplot.dataset import get, resources_list, load_any_resource
+
+
+def test_get_datasets():
+    for file in resources_list:
+        assert get(file)
+
+
+def test_resources_list():
+    for filename in resources_list:
+        get(filename)
+
+
+def test_load_any_resource():
+    for filename in resources_list:
+        data = load_any_resource(filename)
+        assert len(data) > 0
--- ctaplot/tests/test_get_datasets.py
+++
@@ -1,7 +0,0 @@
-from ctaplot.dataset import get, resources_list
-
-
-def test_get_datasets():
-    for file in resources_list:
-        assert get(file)
-
