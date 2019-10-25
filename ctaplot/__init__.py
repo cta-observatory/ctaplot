3ed3e995cca09f183dc64599912918851658f2ee
vuillaut <thomas.vuillaume@gmail.com>
2019-10-25 19:31:39 +0200

    tweek imports

--- ctaplot/__init__.py
+++ ctaplot/__init__.py
@@ -1,7 +1,5 @@
 from ctaplot.plots.plots import *
 from ctaplot.ana.ana import *
-from ctaplot.io.dataset import *
-from ctaplot.io.io import *
 from .gammaboard import gammaboard
 
 __version__ = '0.4.0'
