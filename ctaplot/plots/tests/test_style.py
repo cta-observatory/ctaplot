1467f9b819b76f119ca2c9c60b4ef47ba9c7c5e1
vuillaut <thomas.vuillaume@gmail.com>
2020-11-29 13:29:12 +0100

    test that style is actually applied

--- ctaplot/plots/tests/test_style.py
+++ ctaplot/plots/tests/test_style.py
@@ -3,8 +3,11 @@ import matplotlib.pyplot as plt
 
 def test_set_style():
     style.set_style('slides')
+    assert plt.rcParams['figure.dpi'] == 200.
     style.set_style('paper')
+    assert plt.rcParams['figure.dpi'] == 200.
     style.set_style('notebook')
+    assert plt.rcParams['figure.dpi'] == 200.
 
 def test_context():
     with style.context('slides'):
