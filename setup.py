065c5279f4b5761e9bb2636db4b60efa44f7fd67
Thomas Vuillaume <thomas.vuillaume@gmail.com>
2025-07-11 14:43:15 +0200

    Replace deprecated pkg_resources with importlib.resources (#208)
    * Replace deprecated pkg_resources with importlib.resources
    
    - Replace pkg_resources.resource_exists() with importlib.resources.files() and .is_file()
    - Replace pkg_resources.resource_filename() with importlib.resources.as_file() context manager
    - Fix bare except clause for better exception handling
    - Resolves deprecation warning and future-proofs against pkg_resources removal
    drop support for python < 3.9
    - fixes readthedocs error
    
    Fixes #207

--- setup.py
+++ setup.py
@@ -45,6 +45,9 @@ setup(
     classifiers=[
         'Intended Audience :: Science/Research',
         'Programming Language :: Python :: 3',
+        'Programming Language :: Python :: 3.9',
+        'Programming Language :: Python :: 3.10',
+        'Programming Language :: Python :: 3.11',
         'Topic :: Scientific/Engineering :: Astronomy',
     ],
     data_files=[('ctaplot', dataset), ],
